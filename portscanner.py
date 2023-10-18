3de44b4131699edcf5da5a40a8ba28ffc7220fde
Ardi Sasongko <ardisasongko002@gmail.com>
2023-10-18 19:51:00 +0700

    Firts Commit

---
+++ portscanner.py
@@ -0,0 +1,29 @@
+import socket
+
+def scan(targets, ports):
+    for port in range(1, ports + 1):
+        scan_port(targets, port)
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)  # Menetapkan timeout koneksi
+        result = sock.connect_ex((ipaddress, port))
+
+        if result == 0:
+            print(f"[+] Port {port} is Open")
+        else:
+            print(f"[-] Port {port} is Closed")
+        sock.close()
+    except socket.error:
+        print(f"[-] Port {port} is Closed")
+
+targets = input("[*] Enter Targets To Scan (split them by (,)): ")
+ports = int(input("[*] Enter How Many Ports To Scan: "))
+
+if ',' in targets:
+    print("[*] Scanning multiple targets")
+    for ip_addr in targets.split(','):
+        scan(ip_addr.strip(), ports)
+else:
+    scan(targets, ports)
